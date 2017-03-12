f7171107df6d72ccc1f8c3c7eb9e6759f2c6b958
Alex <alex.barry@gmail.com>
2017-03-12 19:09:06 -0400

    I think I have this cancel stuff figured out, just a little more work to get it up and running.

--- src/Firebase/Authentication/StateChange.elm
+++ src/Firebase/Authentication/StateChange.elm
@@ -43,7 +43,7 @@ subMap func subMsg =
 
 type alias State msg =
     { subs : List (MySub msg)
-    , cancelMethods : Dict String (() -> Task x ())
+    , cancelCallbacks : Dict String (() -> Task x ())
     }
 
 
@@ -51,13 +51,13 @@ init : Task x (State msg)
 init =
     Task.succeed
         { subs = []
-        , cancelMethods = []
+        , cancelCallbacks = []
         }
 
 
 type SelfMsg msg
-    = AddSubscription Auth
-    | RemoveSubscription Auth
+    = AddSubscriptions
+    | RemoveSubscriptions
     | AuthStateChanged (Maybe User)
 
 
@@ -75,6 +75,56 @@ onEffects :
     -> State msg
     -> Task never (State msg)
 onEffects router newSubs oldState =
+    let
+        uniqueSubs : List (MySub msg)
+        uniqueSubs =
+            let
+                initialFold : ( List String, List (MySub msg) )
+                initialFold =
+                    ( [], [] )
+            in
+                newSubs
+                    |> List.foldl foldSubs ( [], [] )
+                    |> Tuple.second
+
+        foldSubs : MySub msg -> ( List String, List (MySub msg) ) -> ( List String, List (MySub msg) )
+        foldSubs mySub ( appNames, approvedSubs ) =
+            let
+                appName : String
+                appName =
+                    case mySub of
+                        MySub auth _ ->
+                            auth
+                                |> Firebase.Authentication.app
+                                |> Firebase.name
+
+                appAlreadyListed : Bool
+                appAlreadyListed =
+                    appNames
+                        |> List.filter ((==) appName)
+                        |> List.isEmpty
+                        |> not
+
+                newAppNames : List String
+                newAppNames =
+                    if appAlreadyListed then
+                        []
+                    else
+                        [ appName ]
+
+                newApprovedSubs : List (MySub msg)
+                newApprovedSubs =
+                    if appAlreadyListed then
+                        []
+                    else
+                        [ mySub ]
+            in
+                ( appNames ++ newAppNames
+                , approvedSubs ++ newApprovedSubs
+                )
+    in
+        Task.succeed { oldState | subs = uniqueSubs }
+            |> Task.andThen (Platform.sendToSelf router AddSubscriptions)
 
 
 onSelfMsg :
@@ -82,4 +132,41 @@ onSelfMsg :
     -> SelfMsg msg
     -> State msg
     -> Task x (State msg)
-onSelfMsg router selfMsg oldState =
+onSelfMsg router selfMsg state =
+    case selfMsg of
+        AddSubscriptions ->
+            let
+                newSubscriptions : List (MySub msg)
+                newSubscriptions =
+                    state.subs
+                        |> List.filter subHasNoCancelCallback
+
+                subHasNoCancelCallback : MySub msg -> Bool
+                subHasNoCancelCallback mySub =
+                    case mySub of
+                        MySub auth _ ->
+                            let
+                                appName : String
+                                appName =
+                                    auth
+                                        |> Firebase.Authentication.app
+                                        |> Firebase.name
+                            in
+                                case (Dict.get appName state.cancelCallbacks) of
+                                    Just _ ->
+                                        False
+
+                                    Nothing ->
+                                        True
+
+                newCancelCallbacks : Dict String (() -> Task x ()) -> Dict String (() -> Task x ())
+                newCancelCallbacks cancelCallbacks =
+                    newSubscriptions
+                        |> List.map Native.Authentication.StateChange.on
+
+            in
+                Task.succeed { state | cancelCallbacks = state.cancelCallbacks ++ newCancelCallbacks }
+
+        RemoveSubscriptions ->
+
+        AuthStateChanged maybeUser ->
