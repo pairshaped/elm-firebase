22b5b31a4db1b1783d3f4708aaafb36617558204
Ryan Barth <ryan@ryportal.com>
2017-05-03 11:15:40 -0700

    change firebase listener events to their own types

--- src/Firebase/Database/Types.elm
+++ src/Firebase/Database/Types.elm
@@ -1,6 +1,16 @@
 module Firebase.Database.Types exposing (..)
 
+{-| Temporary documentation to enable testing.
 
+# Types
+@docs Database
+
+@docs Reference
+
+--}
+
+{-| Somrthisg
+--}
 type Database
     = Database
 
@@ -19,3 +29,16 @@ type Snapshot
 
 type OnDisconnect
     = OnDisconnect
+
+{-| Designates a realtime database event.
+
+See [firebase.database.Reference#on](https://firebase.google.com/docs/reference/node/firebase.database.Reference#on)
+for more information on event types.
+--}
+type Event
+    = Value
+    | ChildAdded
+    | ChildMoved
+    | ChildChanged
+    | ChildRemoved
+
